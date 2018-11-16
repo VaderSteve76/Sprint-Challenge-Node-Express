caf45e4edda8c9d4ce47051c65166987892753a2
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-11-16 13:05:24 -0500

    App & index

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
+import ProjectList from '../components/ProjectList';
+import ActionList from '../components/ActionList';
+
 import './App.css';
 
+
+const url = 'http://localhost:8000';
+
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: [],
+      actions: []
+    }
+  }
+
+  getProjects(){
+    axios
+      .get(`${url}/projects`)
+      .then(res => this.setState({ projects: res.data }))
+      .catch(err => console.log(err));
+    console.log(this.state.projects);
+  }
+
+  getActions(){
+    axios
+      .get(`${url}/actions`)
+      .then(res => this.setState({ actions: res.data }))
+      .catch(err => console.log(err));
+    console.log(this.state.actions);
+  }
+
+  componentDidMount() {
+    this.getProjects();
+    this.getActions();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ProjectList projects={this.state.projects} />
+        <ActionList actions={this.state.actions} />
       </div>
     );
   }
